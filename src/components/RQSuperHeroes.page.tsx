0b77b1380be9bd937473b45f6c47066027c176b9
Aygün Bayır <turkoaygun@gmail.com>
2023-07-24 20:49:44 +0300

    feat: useQuery example

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -1,5 +1,37 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import { SuperHero } from '../types/superhero';
+
 function RQSuperHeroesPage() {
-  return <div>RQ Super Heroes Page</div>;
+  const fetchSuperHeroes = async () => {
+    const data = await axios.get<SuperHero[]>(
+      'http://localhost:4000/superheroes'
+    );
+    return data;
+  };
+
+  const { isLoading, data, error, isError } = useQuery(
+    ['super-heroes'],
+    fetchSuperHeroes
+  );
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError && error instanceof Error) {
+    return <h2>Error: {error && error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data &&
+        data.data.map((hero: SuperHero) => {
+          return <div key={hero.name}>{hero.name}</div>;
+        })}
+    </>
+  );
 }
 
 export default RQSuperHeroesPage;
--- src/components/SuperHeroes.page.tsx
+++ src/components/SuperHeroes.page.tsx
@@ -5,16 +5,27 @@ import { SuperHero } from '../types/superhero';
 function SuperHeroesPage() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [data, setData] = useState<SuperHero[]>([]);
+  const [error, setError] = useState<string>('');
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes').then((res) => {
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get<SuperHero[]>('http://localhost:4000/superheroes')
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((error: Error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error: {error}</h2>;
   }
 
   return (
